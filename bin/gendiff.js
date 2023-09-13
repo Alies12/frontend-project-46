f451f0bafdd183449d08466b83008603f5c6c5ac
Андрей Сингаевский <andrejsingaevskij@MacBook-Air-Andrej.local>
2023-09-13 10:02:58 +0300

    step 2 final

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,7 +2,13 @@ import { Command } from 'commander';
 const program = new Command();
 
 program
-    .name('gendiff-js')
+    .name('gendiff')
+    .description('Compares two configuration files and shows a difference.')
+    .argument('<filepath1>')
+    .argument('<filepath2>')
+    .option('-V, --version', 'output the version number')
+    .helpOption('-h, --help', 'output usage information')
+    .option('-f, --format <type>', 'output format')
     .description('CLI to some JavaScript string utilites')
-    .version('0.0.1');
-program.parse();
+
+    program.parse();
